30694b099a915695dba6228265ed205aeaa614f6
Ghostkeeper <rubend@tutanota.com>
2020-11-14 02:47:31 +0100

    Add benchmarker and basic CMake set-up
    Very basic.

---
+++ include/nadir/benchmarker.hpp
@@ -0,0 +1,24 @@
+/*
+ * Strategy pattern optimiser with compile-time linear regression.
+ * Copyright (C) 2020 Ghostkeeper
+ * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+ * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
+ */
+
+#ifndef NADIR_BENCHMARKER
+#define NADIR_BENCHMARKER
+
+namespace nadir {
+
+/*!
+ * This class allows the user of the Nadir library to create a benchmarking
+ * application with relative ease.
+ */
+class Benchmarker {
+
+};
+
+}
+
+#endif
---
+++ include/nadir/nadir.hpp
@@ -0,0 +1,16 @@
+/*
+ * Strategy pattern optimiser with compile-time linear regression.
+ * Copyright (C) 2020 Ghostkeeper
+ * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+ * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
+ */
+
+#ifndef NADIR_NADIR
+#define NADIR_NADIR
+
+//This header collects all symbols from Nadir. This makes it easier to use, not requiring any internal knowledge of Nadir's file layout.
+
+#include "benchmarker.hpp"
+
+#endif
