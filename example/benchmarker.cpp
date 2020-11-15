4379669b7a665ff4b185df42b1382697166de0c3
Ghostkeeper <rubend@tutanota.com>
2020-11-15 14:44:49 +0100

    Register two options for testing
    Maybe I should factor out the generation of the input problem. We'll see about that later once I've got something semi-working.

--- example/benchmarker.cpp
+++ example/benchmarker.cpp
@@ -6,12 +6,35 @@
  * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
  */
 
-#include <iostream>
-
+#include <random>
 #include <nadir/nadir.hpp>
 #include "sort.hpp"
 
 int main() {
-	nadir::Benchmarker benchmarker;
+	//Create benchmarks with 1 parameter: The size of the data set to sort.
+	nadir::Benchmarker<size_t> benchmarker;
+	benchmarker.add_option("sort_n2", [](size_t problem_size){
+		//Create a random problem, but with a fixed seed so every experiment is the same and fair.
+		std::mt19937 rng(1337);
+		std::vector<int> input;
+		input.reserve(problem_size);
+		for(size_t i = 0; i < problem_size; ++i) {
+			input.push_back(rng());
+		}
+
+		//Execute the code under test.
+		//TODO: Ideally we only want to time this part.
+		example::sort_n2(input);
+	});
+	benchmarker.add_option("sort_nlogn", [](size_t problem_size){
+		std::mt19937 rng(1337);
+		std::vector<int> input;
+		input.reserve(problem_size);
+		for(size_t i = 0; i < problem_size; ++i) {
+			input.push_back(rng());
+		}
+
+		example::sort_nlogn(input);
+	});
 	return 0;
 }
