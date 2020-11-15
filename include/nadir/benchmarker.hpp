c60599659a7ed4f70fc3bd09340f482219628986
Ghostkeeper <rubend@tutanota.com>
2020-11-15 17:38:28 +0100

    Reduce default test size with size_t parameters
    Because sorting 10 million items with an insertion sort is maybe not the best idea.

--- include/nadir/benchmarker.hpp
+++ include/nadir/benchmarker.hpp
@@ -128,7 +128,7 @@ protected:
 	 */
 	void fill_default_range(std::vector<size_t>& range) {
 		range.clear();
-		range.insert(range.end(), {0, 1, 5, 10, 25, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 2500000, 5000000, 10000000});
+		range.insert(range.end(), {0, 1, 5, 10, 25, 50, 100, 500, 1000, 5000, 10000, 50000, 100000});
 	}
 
 	/*!
